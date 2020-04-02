a88ab61598317dc478d3f73e5c677c508582cf72
a <a@a.com>
2020-04-02 10:32:38 -0400

    basic server up and running

--- src/web_server.py
+++ src/web_server.py
@@ -1,28 +1,10 @@
 # Made using https://flask.palletsprojects.com/ as a reference
 
-from flask import Flask, send_from_directory, abort
+from flask import Flask, url_for, redirect
 app = Flask(__name__)
 
-@app.route('/styles/<path:path>')
-def send_style_file(path):
-    try:
-        return send_from_directory('./static/styles', path)
-    except:
-        return abort(404)
 
-@app.route('/images/<path:path>')
-def send_image_file(path):
-    try:
-        return send_from_directory('./static/images', path)
-    except:
-        return abort(404)
+@app.route('/')
+def send_home_page():
+  return redirect(url_for('static', filename='index.html'))
 
-@app.route('/html/<path:path>')
-def send_html_file(path):
-    try:
-        return send_from_directory('./static/html', path)
-    except:
-        return abort(404)
-
-if __name__ == "__main__":
-    app.run(port=8000)
