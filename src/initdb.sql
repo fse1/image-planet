912d49e068993d375ebf452f4bffe2e948ef7d8a
a <a@a.com>
2020-04-08 16:25:03 -0400

    added basic database setup

---
+++ src/initdb.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS imageplanet;
+USE imageplanet;
+CREATE TABLE IF NOT EXISTS users (userid BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                                    username TEXT UNIQUE NOT NULL,
+                                    profilepic BIGINT,
+                                    profiledesc TEXT,
+                                    salt TEXT NOT NULL,
+                                    passhash TEXT NOT NULL,
+                                    sessioncookiehash TEXT UNIQUE,
+                                    sessionexpiration DATE,
+                                    csrftoken TEXT UNIQUE);
+CREATE TABLE IF NOT EXISTS images (imageid BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                                     imgtitle TEXT NOT NULL,
+                                     userid BIGINT NOT NULL,
+                                     imgfile TEXT NOT NULL,
+                                     imgdesc TEXT,
+                                     likes BIGINT NOT NULL);
+CREATE TABLE IF NOT EXISTS comments (imageid BIGINT NOT NULL,
+                                       userid BIGINT NOT NULL,
+                                       comtext TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS directmsg (dmid BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                                        lowuserid BIGINT NOT NULL,
+                                        highuserid BIGINT NOT NULL);
+CREATE TABLE IF NOT EXISTS messages (dmid BIGINT NOT NULL,
+                                       userid BIGINT NOT NULL,
+                                       msgtext TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS followers (userid BIGINT NOT NULL,
+                                        followingthisuserid BIGINT NOT NULL);
+CREATE TABLE IF NOT EXISTS likes (userid BIGINT NOT NULL,
+                                    likesthisimageid BIGINT NOT NULL);
--- src/web_server.py
+++ src/web_server.py
@@ -1,13 +1,54 @@
 # Made using https://flask.palletsprojects.com/ as a reference
 
-from flask import Flask, url_for, redirect
+import sys
+import os
+from flask import Flask, url_for, redirect, g
+import mysql.connector
+
+# try and get the database password from env variable
+try:
+  os.environ['DB_PASS']
+except Exception as ex:
+  sys.stderr.write('Cannot find environment variable "DB_PASS"\n')
+  raise ex
+
+# create and configure the Flask application
 app = Flask(__name__)
+app.config.update(DB_USER='root', DB_HOST='127.0.0.1', DB_PASS=(os.environ['DB_PASS']))
+
 
 
 @app.route('/')
 def send_home_page():
   return redirect(url_for('static', filename='index.html'))
 
-# remove for deployment
-if __name__ == "__main__":
-    app.run()
+@app.cli.command('init-db')
+def init_database():
+  try:
+    db = get_database()
+    db_cursor = db.cursor()
+    with app.open_resource('initdb.sql') as schema:
+      commands = schema.read().decode('utf-8')
+      for e in db_cursor.execute(commands, multi=True):
+        pass
+    db.commit()
+    db.close()
+  except Exception as ex:
+    sys.stderr.write('Error Setting Up Database\n')
+    raise ex
+  
+@app.teardown_appcontext
+def close_db(arg):
+  if 'db' in g:
+    g.db.close()
+  
+# grab a copy of the database  
+def get_database():
+  try:
+    db = mysql.connector.connect(host=app.config['DB_HOST'], user=app.config['DB_USER'], password=app.config['DB_PASS'])
+  except Exception as ex:
+    sys.stderr.write('Error Connecting to Database\n')
+    raise ex
+    
+  return db
+  
